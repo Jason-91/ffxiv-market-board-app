2777f844dbffdf798980ff01531e683baf370d1c
Jason Chang <hyunjoon1991@gmail.com>
2025-01-13 16:02:24 -0800

    Got rid of useEffect redundancy in main

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,15 @@
-import React, { StrictMode } from 'react';
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { Provider, useSelector, useDispatch } from 'react-redux';
+import { Provider, useSelector } from 'react-redux';
 import { ThemeProvider, CssBaseline } from '@mui/material';
 import App from './App.tsx';
 import { store } from './store';
 import createCustomTheme from './theme';
 import { RootState } from './store';
-import { setTheme } from './store/themeSlice';
 
 const RootApp = () => {
     const mode = useSelector((state: RootState) => state.theme.mode);
-    const dispatch = useDispatch();
     const theme = createCustomTheme(mode);
-
-    React.useEffect(() => {
-        dispatch(setTheme(mode));
-    }, []);
 
     return (
         <ThemeProvider theme={theme}>
