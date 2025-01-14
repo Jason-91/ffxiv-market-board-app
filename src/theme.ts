8b43b690eacc1b9e8ee7d5ace5a5df09b22820b6
Jason Chang <hyunjoon1991@gmail.com>
2025-01-13 16:03:05 -0800

    Fixed deprecated Grid import

--- src/theme.ts
+++ src/theme.ts
@@ -1,37 +1,35 @@
 import { createTheme } from '@mui/material/styles';
 
 const createCustomTheme = (mode: 'light' | 'dark') =>
-  createTheme({
-    palette: {
-      mode,
-      ...(mode === 'light'
-        ? {
-            // palette values for light mode
-            primary: { main: '#1976d2' },
-            secondary: { main: '#9c27b0' },
-            background: {
-              default: '#fff',
-              paper: '#fff',
-            },
-            text: {
-              primary: '#000',
-              secondary: '#757575',
-            },
-          }
-        : {
-            // palette values for dark mode
-            primary: { main: '#90caf9' },
-            secondary: { main: '#ce93d8' },
-            background: {
-              default: '#121212',
-              paper: '#1e1e1e',
-            },
-            text: {
-              primary: '#fff',
-              secondary: '#bdbdbd',
-            },
-          }),
-    },
-  });
+    createTheme({
+        palette: {
+            mode,
+            ...(mode === 'light'
+                ? {
+                    primary: { main: '#1976d2' },
+                    secondary: { main: '#9c27b0' },
+                    background: {
+                        default: '#fff',
+                        paper: '#fff',
+                    },
+                    text: {
+                        primary: '#000',
+                        secondary: '#757575',
+                    },
+                }
+                : {
+                    primary: { main: '#90caf9' },
+                    secondary: { main: '#ce93d8' },
+                    background: {
+                        default: '#121212',
+                        paper: '#1e1e1e',
+                    },
+                    text: {
+                        primary: '#fff',
+                        secondary: '#bdbdbd',
+                    },
+                }),
+        },
+    });
 
 export default createCustomTheme;
